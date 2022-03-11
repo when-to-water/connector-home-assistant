66f0922674fa032f909daaeddb57aee514f22837
Mekcyed <maximilian.deycke@gmail.com>
2022-03-11 23:15:48 +0100

    feat: ✨ created conn to mariadb

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print( "hello world")
+# Module imports
+import mariadb
+import sys
+import os
+
+# Connect to MariaDB Platform
+try:
+    conn = mariadb.connect(
+        user=os.environ.get('MARIADB_USER'),
+        password=os.environ.get('MARIADB_KEY'),
+        host=os.environ.get('MARIADB_HOST'),
+        port=int(os.environ.get('MARIADB_PORT')),
+        database=os.environ.get('MARIADB_DATABASE')
+    )
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
+
+# Get Cursor
+cur = conn.cursor()
+
+# SQL
+
+cur.execute('Select distinct entity_id FROM states')
+
+for entity_id in cur:
+   print(f"{entity_id}")
+
+conn.close()
