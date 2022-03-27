974f6a1f90c9619292d1ff8fa8967436d4383125
BeneKenobi <1708125+BeneKenobi@users.noreply.github.com>
2022-03-27 18:05:12 +0200

    style: 🎨 format with `black`

--- main.py
+++ main.py
@@ -4,17 +4,29 @@ import sqlalchemy
 from sqlalchemy import create_engine
 import pandas as pd
 
-strEngine = "mariadb+mariadbconnector://" + os.environ.get('MARIADB_USER') + ":" + os.environ.get('MARIADB_KEY') + "@" + \
-os.environ.get('MARIADB_HOST') + ":" + os.environ.get('MARIADB_PORT') + "/" + os.environ.get('MARIADB_DATABASE')
+strEngine = (
+    "mariadb+mariadbconnector://"
+    + os.environ.get("MARIADB_USER")
+    + ":"
+    + os.environ.get("MARIADB_KEY")
+    + "@"
+    + os.environ.get("MARIADB_HOST")
+    + ":"
+    + os.environ.get("MARIADB_PORT")
+    + "/"
+    + os.environ.get("MARIADB_DATABASE")
+)
 
 engine = sqlalchemy.create_engine(strEngine)
 
-# SQL Statement
-df = pd.read_sql("SELECT state_id,entity_id,state,attributes, \
+df = pd.read_sql(
+    "SELECT state_id,entity_id,state,attributes, \
 last_changed,last_updated,created,old_state_id \
 FROM states \
 WHERE entity_id LIKE '%efekta%'and state <>'unavailable' \
-ORDER BY `states`.`created`  DESC",engine)
+ORDER BY `states`.`created`  DESC",
+    engine,
+)
 
-#Results 
+# Results
 print(df.head(5))
