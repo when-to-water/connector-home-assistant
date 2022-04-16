fa0d77dbc35f5e5ea7874b023e19fbc50f823439
Mekcyed <maximilian.deycke@gmail.com>
2022-04-16 17:47:43 +0200

    refactor: ➕ switched to pymysql lib

--- connectors/homeassistant_recorder/main.py
+++ connectors/homeassistant_recorder/main.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 if os.getenv("WTW_CONNECTOR_TYPE") == "HOMEASSISTANT_RECORDER_MARIADB":
     strEngine = (
-        f"mariadb+mariadbconnector://{os.getenv('WTW_CONNECTOR_HOMEASSISTANT_RECORDER_USER')}"
+        f"mariadb+pymysql://{os.getenv('WTW_CONNECTOR_HOMEASSISTANT_RECORDER_USER')}"
         f":{os.getenv('WTW_CONNECTOR_HOMEASSISTANT_RECORDER_KEY')}"
         f"@{os.getenv('WTW_CONNECTOR_HOMEASSISTANT_RECORDER_HOST')}:{os.getenv('WTW_CONNECTOR_HOMEASSISTANT_RECORDER_PORT')}"
         f"/{os.getenv('WTW_CONNECTOR_HOMEASSISTANT_RECORDER_DATABASE')}"
@@ -23,7 +23,7 @@ df = pd.read_sql(
         "SELECT state_id,entity_id,state,attributes, "
         "last_changed,last_updated,created,old_state_id "
         "FROM states "
-        "WHERE entity_id LIKE '%efekta%'and state <>'unavailable' "
+        "WHERE entity_id LIKE '%%efekta%%'and state <>'unavailable' "
         "ORDER BY `states`.`created` DESC"
     ),
     engine,
