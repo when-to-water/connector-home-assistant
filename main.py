e1b5b8c750c6cc0d06371d9bebac161ff67be498
Mekcyed <maximilian.deycke@gmail.com>
2022-04-28 20:00:29 +0200

    feat: ✨ added data formatting for timesteam db

--- main.py
+++ main.py
@@ -1,33 +1,43 @@
 import os
 import sqlalchemy
 import pandas as pd
+import ast
 from dotenv import load_dotenv
 
 
-load_dotenv()
+def main():
 
-if os.getenv("WTW_CONNECTOR_TYPE") == "HOMEASSISTANT_RECORDER_MARIADB":
-    strEngine = (
-        f"mariadb+pymysql://{os.getenv('WTW_CONNECTOR_HOMEASSISTANT_RECORDER_USER')}"
-        f":{os.getenv('WTW_CONNECTOR_HOMEASSISTANT_RECORDER_KEY')}"
-        f"@{os.getenv('WTW_CONNECTOR_HOMEASSISTANT_RECORDER_HOST')}:{os.getenv('WTW_CONNECTOR_HOMEASSISTANT_RECORDER_PORT')}"
-        f"/{os.getenv('WTW_CONNECTOR_HOMEASSISTANT_RECORDER_DATABASE')}"
+    load_dotenv()
+
+    if os.getenv("WTW_CONNECTOR_TYPE") == "HOMEASSISTANT_RECORDER_MARIADB":
+        strEngine = (
+            f"mariadb+pymysql://{os.getenv('WTW_CONNECTOR_HOMEASSISTANT_RECORDER_USER')}"
+            f":{os.getenv('WTW_CONNECTOR_HOMEASSISTANT_RECORDER_KEY')}"
+            f"@{os.getenv('WTW_CONNECTOR_HOMEASSISTANT_RECORDER_HOST')}:{os.getenv('WTW_CONNECTOR_HOMEASSISTANT_RECORDER_PORT')}"
+            f"/{os.getenv('WTW_CONNECTOR_HOMEASSISTANT_RECORDER_DATABASE')}"
+        )
+    else:
+        exit(f"Connector type '{os.getenv('WTW_CONNECTOR_TYPE')}' not supported")
+
+    engine = sqlalchemy.create_engine(strEngine)
+
+    df = pd.read_sql(
+        (
+            "SELECT state_id,state as measure_value,attributes, created "
+            "FROM states "
+            "WHERE entity_id LIKE '%%efekta%%'and state <>'unavailable' "
+            "ORDER BY `states`.`created` DESC"
+        ),
+        engine,
     )
-else:
-    exit(f"Connector type '{os.getenv('WTW_CONNECTOR_TYPE')}' not supported")
 
-engine = sqlalchemy.create_engine(strEngine)
+    dftemp = df['attributes'].apply(lambda x: ast.literal_eval(x))
+    df['measure_name'] = dftemp.apply(lambda x: x.get('device_class'))
+    df['unit'] = dftemp.apply(lambda x: x.get('unit_of_measurement'))
+    df['friendly_name'] = dftemp.apply(lambda x: x.get('friendly_name'))
+    df['friendly_name'] = df['friendly_name'].str[:5]
+    df.drop('attributes', inplace=True, axis=1)
+    print(df.head(100))
 
-df = pd.read_sql(
-    (
-        "SELECT state_id,entity_id,state,attributes, "
-        "last_changed,last_updated,created,old_state_id "
-        "FROM states "
-        "WHERE entity_id LIKE '%%efekta%%'and state <>'unavailable' "
-        "ORDER BY `states`.`created` DESC"
-    ),
-    engine,
-)
 
-# Results
-print(df.head(5))
+main()
