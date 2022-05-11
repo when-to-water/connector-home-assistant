8ba7c8d34c321e0af3699a4d495ecff5a590d984
Mekcyed <maximilian.deycke@gmail.com>
2022-05-11 21:02:36 +0200

    feat: 🚧 added connection to timestream db

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ import sqlalchemy
 import pandas as pd
 import ast
 from dotenv import load_dotenv
+from datetime import datetime
+import boto3
 
 
 def main():
-
     load_dotenv()
+    latest_timestamp = get_latest_timestamp()
+    get_homeassistant_data(latest_timestamp)
 
+def get_homeassistant_data(latest_timestamp):
     if os.getenv("WTW_CONNECTOR_TYPE") == "HOMEASSISTANT_RECORDER_MARIADB":
         strEngine = (
             f"mariadb+pymysql://{os.getenv('WTW_CONNECTOR_HOMEASSISTANT_RECORDER_USER')}"
@@ -23,21 +27,40 @@ def main():
 
     df = pd.read_sql(
         (
-            "SELECT state_id,state as measure_value,attributes, created "
+            "SELECT state_id,state as measure_value,attributes, created as timestamp "
             "FROM states "
             "WHERE entity_id LIKE '%%efekta%%'and state <>'unavailable' "
-            "ORDER BY `states`.`created` DESC"
+            f"AND created > '{latest_timestamp}' "
+            "ORDER BY `states`.`created` ASC"
         ),
         engine,
     )
 
-    dftemp = df['attributes'].apply(lambda x: ast.literal_eval(x))
-    df['measure_name'] = dftemp.apply(lambda x: x.get('device_class'))
-    df['unit'] = dftemp.apply(lambda x: x.get('unit_of_measurement'))
-    df['friendly_name'] = dftemp.apply(lambda x: x.get('friendly_name'))
-    df['friendly_name'] = df['friendly_name'].str[:5]
-    df.drop('attributes', inplace=True, axis=1)
-    print(df.head(100))
+    dftemp = df["attributes"].apply(lambda x: ast.literal_eval(x))
+    df["measure_name"] = dftemp.apply(lambda x: x.get("device_class"))
+    df["unit"] = dftemp.apply(lambda x: x.get("unit_of_measurement"))
+    df["friendly_name"] = dftemp.apply(lambda x: x.get("friendly_name"))
+    df["friendly_name"] = df["friendly_name"].str[:5]
+    df.drop("attributes", inplace=True, axis=1)
+    print(df['measure_name'].unique())
+    
+
+def get_latest_timestamp():
+
+    read_client = boto3.client("timestream-query")
+    response = ""
+
+    try:
+        response = read_client.query(
+            QueryString="SELECT Max(time) AS MaxTimestamp FROM \"when-to-water\".\"sensor-data\""
+        )
+    except Exception as e:
+        print(f"An exception occured: {e}")
+
+    if response['Rows'][0]['Data'][0]['NullValue'] == True:
+        return datetime.fromtimestamp(0)
+
+    print(response)
 
 
 main()
