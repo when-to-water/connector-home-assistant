58bd96d06208851cb2dc4d5adc69b5f8c101cc68
Mekcyed <maximilian.deycke@gmail.com>
2022-05-13 00:36:43 +0200

    feat: 🚧 write to timestream still not working

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ import boto3
 def main():
     load_dotenv()
     latest_timestamp = get_latest_timestamp()
-    get_homeassistant_data(latest_timestamp)
+    df = get_homeassistant_data(latest_timestamp)
+    sent_data_to_timestream(df.head(10))
 
 
 def get_homeassistant_data(latest_timestamp):
@@ -26,6 +27,7 @@ def get_homeassistant_data(latest_timestamp):
 
     engine = sqlalchemy.create_engine(strEngine)
 
+<<<<<<< Updated upstream
     df = pd.read_sql(
         (
             "SELECT state_id,state as measure_value,attributes, created as timestamp "
@@ -37,14 +39,32 @@ def get_homeassistant_data(latest_timestamp):
         engine,
         params={"latest_timestamp": latest_timestamp},
     )
+=======
+    try:
+        df = pd.read_sql(
+            (
+                "SELECT state as MeasureValue,attributes, FLOOR(UNIX_TIMESTAMP(created)) as Time "
+                "FROM states "
+                "WHERE entity_id LIKE '%%efekta%%'and state <>'unavailable' "
+                f"AND created > '{latest_timestamp}' "
+                "ORDER BY `states`.`created` ASC"
+            ),
+            engine,
+        )
+    except Exception as e:
+        print(f"An exception occured: {e}")
+>>>>>>> Stashed changes
 
     dftemp = df["attributes"].apply(lambda x: ast.literal_eval(x))
-    df["measure_name"] = dftemp.apply(lambda x: x.get("device_class"))
-    df["unit"] = dftemp.apply(lambda x: x.get("unit_of_measurement"))
-    df["friendly_name"] = dftemp.apply(lambda x: x.get("friendly_name"))
-    df["friendly_name"] = df["friendly_name"].str[:5]
+    df["Unit"] = dftemp.apply(lambda x: x.get("unit_of_measurement"))
+    df["SensorName"] = dftemp.apply(lambda x: x.get("friendly_name"))
+    df["MeasureName"] = df["SensorName"].str.split(" ").str[1]
+    df["SensorName"] = df["SensorName"].str[:5]
+    df["Time"] = df["Time"].apply(str)
+    df = df[df['SensorName'].isin(['PWS_1', 'PWS_2', 'PWS_3'])]
     df.drop("attributes", inplace=True, axis=1)
-    print(df["measure_name"].unique())
+    print(df.head())
+    return df
 
 
 def get_latest_timestamp():
@@ -62,7 +82,35 @@ def get_latest_timestamp():
     if response["Rows"][0]["Data"][0]["NullValue"] is True:
         return datetime.fromtimestamp(0)
 
-    print(response)
+
+def sent_data_to_timestream(df):
+
+    write_client = boto3.client("timestream-write")
+    DB_NAME = "when-to-water"
+    TBL_NAME = "sensor-data"
+    response = ""
+
+    for x in pd.unique(df["SensorName"]):
+        for y in pd.unique(df["Unit"]):
+            records = []
+            common_attributes = {'Dimensions': [{'Name': 'SensorName', 'Value': x}, {'Name': 'Unit', 'Value': y}], 'MeasureValueType': 'DOUBLE'}
+            dftemp = df[(df['SensorName'] == x) & (df['Unit'] == y)]
+            dftemp = dftemp.drop(columns=["SensorName", "Unit"])
+            records = dftemp.to_dict(orient="records")
+            print(common_attributes)
+
+    try:
+        response = write_client.write_records(
+            DatabaseName=DB_NAME, TableName=TBL_NAME, Records=records, CommonAttributes=common_attributes
+        )
+        print("WriteRecords Status: [%s]" % response['ResponseMetadata']['HTTPStatusCode'])
+    except write_client.exceptions.RejectedRecordsException as e:
+        print("RejectedRecords: ", e)
+        for rr in e.response["RejectedRecords"]:
+            print("Rejected Index " + str(rr["RecordIndex"]) + ": " + rr["Reason"])
+        print("Other records were written successfully. ")
+    except Exception as e:
+        print(f"An exception occured: {e}")
 
 
 main()
